5c0902e85dc1a8f5d470fbb63fd8c68772e7dccc
cnow <siddhu.vsss@gmail.com>
2023-09-19 11:02:00 -0400

    added User and Post types

---
+++ types/types.ts
@@ -0,0 +1,17 @@
+export type Post = {
+    author: string,
+    content: string, 
+    date: string,
+}
+
+
+export type User = {
+    id: string,
+    email: string,
+    followers: string[],
+    name: string,
+    password: string,
+    posts: Post[],
+    username: string,
+
+}
